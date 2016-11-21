c25d7131b0379651a329d54377b86852f8b403e8
theQmech <rupanshu.ganvir@gmail.com>
2016-11-21 20:55:19 +0530

    Database

--- create_tables.sql
+++ create_tables.sql
@@ -1,38 +1,31 @@
-drop table if exists showroom, ownership, rentdata, bike_at_stand, bike, biker, bikestand;
-drop type if exists status_, ownedby_, usage_, reason_, request_, latlong_;
+drop schema if exists public cascade;
+create schema public;
 
+create type ride_ as enum('bike', 'car', 'mobike');
 create type ownedby_ as enum('company', 'user');
-create type status_ as enum('with_owner', 'unusable', 'usable');
-create type usage_ as enum('on_rent', 'available_for_renting', 'available_for_buying');
-create type reason_ as enum('missing', 'undecided', 'repairing', 'damaged');
-create type request_ as enum('nullable', 'repair', 'sell', 'lend');
+create type status_ as enum('with_owner', 'lent');
+create type request_ as enum('nullable', 'repair', 'unlend', 'lend');
 create type latlong_ as (lat float, long float);
 
-/* this table needs serious reviews
- * valid_from etc also need to be included in primary key
- */
-
-create table bike(
-	bikeid			varchar(20) primary key not null,
+create table ride(
+	rideid			varchar(20) primary key not null,
+	ridetype		ride_,
 	makemodel		varchar(20),
 	color			varchar(20),
-	ownedby			ownedby_,
-	status			status_,
-	usage			usage_,
-	reason			reason_,
-	request			request_
+	ownedby			ownedby_ not null,
+	status			status_ not null
 );
 
-create table biker(
-	userid			varchar(20) primary key not null,
+create table rider(
+	riderid			varchar(20) primary key not null,
 	name			varchar(40),
 	password		varchar(20),
 	email			varchar(20),
 	address			varchar(60)
 );
 
-create table bikestand(
-	bikestandid		varchar(20) primary key not null,
+create table stand(
+	standid			varchar(20) primary key not null,
 	capacity		int			not null,
 	address			varchar(60)	not null,
 	location		latlong_	not null
@@ -47,28 +40,38 @@ create table showroom(
 );
 
 create table ownership(
-	bikeid			varchar(20)	not null,
+	rideid			varchar(20)	not null,
 	ownerid			varchar(20)	not null,
-	primary key (bikeid, ownerid),
-	foreign key (bikeid) references bike on DELETE cascade,
-	foreign key (ownerid) references biker on DELETE cascade
+	primary key (rideid, ownerid),
+	foreign key (rideid) references ride on DELETE cascade,
+	foreign key (ownerid) references rider on DELETE cascade
 );
 
 create table rentdata(
-	bikeid			varchar(20) primary key not null,
+	rideid			varchar(20) primary key not null,
 	userid			varchar(20) not null,
-	frombikestandid	varchar(20) not null,
-	tobikestandid	varchar(20),
-	foreign key (bikeid) references bike on DELETE cascade,
-	foreign key (userid) references biker on DELETE cascade,
-	foreign key (frombikestandid) references bikestand on DELETE cascade,
-	foreign key (tobikestandid) references bikestand on DELETE cascade
+	fromstandid		varchar(20) not null,
+	tostandid		varchar(20),
+	foreign key (rideid) references ride on DELETE cascade,
+	foreign key (userid) references rider on DELETE cascade,
+	foreign key (fromstandid) references stand on DELETE cascade,
+	foreign key (tostandid) references stand on DELETE cascade
 );
 
-create table bike_at_stand(
-	bikeid			varchar(20)	not null,
-	bikestandid		varchar(20)	not null,
-	primary key (bikeid, bikestandid),
-	foreign key (bikeid) references bike on DELETE cascade,
-	foreign key (bikestandid) references bikestand on DELETE cascade
+create table ride_at_stand(
+	rideid			varchar(20)	not null,
+	standid			varchar(20)	not null,
+	primary key (rideid, standid),
+	foreign key (rideid) references ride on DELETE cascade,
+	foreign key (standid) references stand on DELETE cascade
+);
+
+create table requests(
+	rideid			varchar(20)	not null,
+	riderid			varchar(20) not null,
+	type			request_,
+	standid			varchar(20),
+	primary key (rideid, riderid),
+	foreign key (rideid) references ride on DELETE cascade,
+	foreign key (riderid) references rider on DELETE cascade
 );
--- insert_small_data.sql
+++ insert_small_data.sql
@@ -1,21 +1,43 @@
-delete from bike;
-delete from biker;
-delete from bikestand;
+delete from ride;
+delete from rider;
+delete from stand;
 delete from showroom;
 delete from ownership;
 delete from rentdata;
+delete from requests;
 
 -- Add sample data
-insert into bike values ('b1', 'btwin', 'white', 'company', 'usable', 'available_for_renting', null, null);
-insert into bike values ('b2', 'btwin', 'white', 'company', 'usable', 'available_for_renting', null, null);
-insert into bike values ('b3', 'btwin', 'white', 'company', 'usable', 'available_for_renting', null, null);
+insert into ride values ('b1', 'bike', 'btwin', 'White', 'company', 'lent');
+insert into ride values ('b2', 'bike', 'myBike', 'Black', 'user', 'lent');
+insert into ride values ('b3', 'bike', 'Hercules', 'Brown', 'user', 'lent');
+insert into ride values ('b4', 'bike', 'Hero', 'Black', 'user', 'lent');
+insert into ride values ('b5', 'bike', 'Firefox', 'Black', 'user', 'lent');
+insert into ride values ('b6', 'bike', 'Kross', 'Black', 'user', 'lent');
 
-insert into biker values ('u1', 'Shubham', 'pass', 'sgoel160@gmail.com', 'Hostel 3');
-insert into ownership values ('b1', 'u1');
+insert into rider values ('u1', 'Shubham', 'pass', 'sgoel160@gmail.com', 'Hostel 3');
 
-insert into bikestand values ('s1', 20, 'Hostel 4', (0.0,0.0));
-insert into bikestand values ('s2', 20, 'Hostel 5', (0.0,1.0));
+insert into stand values ('s1', 20, 'Hostel 4', (0.0,0.0));
+insert into stand values ('s2', 20, 'Hostel 5', (0.0,1.0));
+insert into stand values ('s3', 20, 'Hostel 6', (0.0,1.0));
+insert into stand values ('s4', 20, 'Hostel 7', (0.0,1.0));
+insert into stand values ('s5', 20, 'Hostel 8', (0.0,1.0));
+insert into stand values ('s6', 20, 'Hostel 9', (0.0,1.0));
+insert into stand values ('s7', 20, 'Hostel 10', (0.0,1.0));
+insert into stand values ('s8', 20, 'Hostel 11', (0.0,1.0));
+insert into stand values ('s9', 20, 'Hostel 12', (0.0,1.0));
+insert into stand values ('s10', 20, 'Hostel 13', (0.0,1.0));
+insert into stand values ('s101', 20, 'Bandra', (0.0,1.0));
+insert into stand values ('s102', 20, 'Dadar', (0.0,1.0));
+insert into stand values ('s103', 20, 'Andheri', (0.0,1.0));
+insert into stand values ('s104', 20, 'CST', (0.0,1.0));
+insert into stand values ('s105', 20, 'Powai', (0.0,1.0));
+insert into stand values ('s106', 20, 'Borivali', (0.0,1.0));
 
-insert into bike_at_stand values ('b1', 's1');
-insert into bike_at_stand values ('b2', 's2');
-insert into bike_at_stand values ('b3', 's2');
+insert into ride_at_stand values ('b1', 's1');
+insert into ride_at_stand values ('b2', 's10');
+insert into ride_at_stand values ('b3', 's2');
+insert into ride_at_stand values ('b4', 's1');
+insert into ride_at_stand values ('b5', 's3');
+insert into ride_at_stand values ('b6', 's1');
+
+insert into ownership values ('b2','u1');
